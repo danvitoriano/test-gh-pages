339fb87b08fa9c3eb76a21fff607188a6cfab876
Danilo Vitoriano <danilo.vitoriano@oi.net.br>
2017-05-17 14:25:05 -0300

    build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter basename='/awesome-idea-box'>
+  <BrowserRouter basename='/test-gh-pages'>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
